c3e53d5f17fceb203275af44862f127b0ac19229
Sospeter Mongare <sosmongare@gmail.com>
2023-08-11 13:58:29 +0300

    Working with states and effects

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 
 import './App.css';
+ import {useState, useEffect} from "react";
 
 // array destructuring
-const [firstCity, secondCity] = ["Mombasa","Nakuru","Nairobi","Kisumu"];
+// const [firstCity, secondCity] = ["Mombasa","Nakuru","Nairobi","Kisumu"];
 
-console.log(firstCity);
-console.log(secondCity);
-function App({library}) {
+// console.log(firstCity);
+// console.log(secondCity);
+// function App({library}) {
+//   return (
+//     <div className="App">
+//       <h1>Hello from {library}</h1>
+//     </div>
+//   );
+// }
+
+// Using states to dynamically change text
+function App() {
+  const [emotion, setEmotion] = useState("happy");
+
+  //side effects
+  useEffect(() => {
+    console.log(`Its ${emotion} around here!`)
+  });
   return (
     <div className="App">
-      <h1>Hello from {library}</h1>
+      <h1>Current Emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("Excited")}>Excited</button>
     </div>
-  );
+  )
 }
-
 export default App;
